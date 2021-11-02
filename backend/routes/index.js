8d186bd5d2d7da28d4f865f5e721c16ec9214c6b
jas0123uah <jas0123@uah.edu>
2021-11-01 22:39:39 -0400

    init commit

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+//
+//
+
+const csurf = require('csurf');
+
+//
+
+//
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+module.exports = router;
